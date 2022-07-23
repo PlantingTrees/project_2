ea984decb617154000f2a26246520b9ceec29461
PlantingTrees <kingsleyob2@gmail.com>
2022-07-23 11:02:23 -0400

    working cpp codde

--- main.cc
+++ main.cc
@@ -9,16 +9,18 @@
 // D -> 0 | 1 | ... | 8 | 9
 
 //Todo! create a text file called input.txt and store these grammar on it.
-//Do we have to be concerned about white space  
+//Do we have to be concerned about white space
 //We may have to remove white spaces from buffers
 
 #include <iostream>
 #include <string>
 #include <fstream>
 
+
 using namespace std;
 
 string buff;
+
 
 bool A(void);
 bool E(void);
@@ -31,157 +33,168 @@ bool L(void);
 bool D(void);
 
 int main(int argc, char *argsv[]){
-    
-    
-    //read the input file 
+   
+    //read the input file
     ifstream fin("input.txt");
-
-    //check if the file is open and ready to read if not cout cannot be read
+    
     if(fin.is_open()){
-        while (fin.good()){ //checks if at the end of the file. once it hits/had read to the end of the file, it will break the while loop
-            //get a line and parse it from there
-            getline(fin, buff);
+        
+        while(fin.good()){
+            getline(fin, buff); //gotten one line from input file
             string t = buff;
-            //begin to parse
-            if (A()){
-                cout<< t << "is in the language"<<endl;
-            }else{
-                cout<< t << "is not in the language"<<endl;
+            //perform parsing
+            if(A()){
+                cout << "The string "<< t << " is in the language.\n" <<endl;
+            }
+            else {
+                
+                cout << "The string "<< t << " is not the language.\n" <<endl;
+                
             }
         }
+        
     }
     else{
-        cout << "Input file cannot be read."<<endl;
+        cout << "The input file cannot be read." <<endl;
     }
-
-     return 0;
-
 }
 
 bool A(void){
     // A -> I = E
-
-    if(I()) {
+    if(I()){
         if(buff[0] == '='){
+            buff = buff.substr(1);
             if(E()){
                 return true;
             }
+            return false;
         }
     }
     return false;
 }
 
-bool I(void){
-   // I -> C | CI
-   if(C()){
-        buff = buff.substr(1);
-        return true;
-    }
-       else if (C()){
+bool I(){
+    // I -> C | CI
+    if(C()){
+        if(buff[0] != '='){
             if(I()){
-                buff =  buff.substr(1); //move to the next item 
                 return true;
             }
-            
+            return false;
         }
-   
-   return false;
+        return true;
+    }
+    return false;
+       
 }
 
-bool C(void){
-    if ('a' <= buff[0] || buff[0] <= 'z'){
+bool C(){
+    if('a' <= buff[0] && buff[0] <= 'z'){
         buff = buff.substr(1);
         return true;
     }
     return false;
 }
 
-bool E(void){
-   // E -> P O P | P
-   if (P()){ // test for P        ===
-        if (O()){ // test for O .  ==  Following the specified rules.
-            if (P()){ // test for P ==
-                buff = buff.substr(1);
+bool E(){
+    // E -> P O P | P
+    
+    if(P()){
+        //buff = buff.substr(1);
+        if(O()){
+            //buff = buff.substr(1); //having a bug here with moving to the next item
+            if(P()){
                 return true;
             }
         }
-   }
-   return false;
+        return true;
+    }
+    return false;
 }
 
-bool P(void){
+bool P(){
     // P -> I | L | UI | UL | (E)
-    if (I() || L()){
-        buff = buff.substr(1);
+    if(I()){
+        return true;
+    }
+    else if(L()){
         return true;
     }
     else if(U()){
-            if(I()){
-                buff = buff.substr(1);
-                return true;
-            }
-            
-        }
-    else if(U()){
-            if(L()){
-                buff = buff.substr(1);
-            }
+        if(L()){
             return true;
-    }
-    else if(buff[0] == '('){
-            buff = buff.substr(1);
-            if (E()){
-                if(buff[0] == ')'){
-                    buff = buff.substr(1);
-                    return true;
-                }
-            }
         }
         return false;
- }
-    
-
-bool L(void){
-    // L => D | DL
-    if(D()) {
-        return true;
     }
-    else if(D()){
-        if(L()){
-            buff = buff.substr(1);
+    else if(U()){
+        if(I()){
             return true;
         }
+        return false;
     }
-
+    else if(buff[0] == '('){
+        buff = buff.substr(1);
+        if(E()){
+            if(buff[0] == ')'){
+                return true;
+            }
+        }
+    }
     return false;
 }
-bool O(void){
+
+bool O(){
     // O -> + | - | * | / | **
-   // if(buff[0] == '+' || buff[0] == '-' || buff[0] == '*' || buff[0] == '/' || buff[0] == '**'){
-     //   buff = buff.substr(1);
-       return true;
-    //}
-    
-    //return false;
+    if(buff[0] == '+'){
+        return true;
+    }
+    else if(buff[0] == '-'){
+        return true;
+    }
+    else if (buff[0] == '*'){
+        return true;
+    }
+    else if(buff[0] == '/'){
+        return true;
+    }
+    else if (buff.compare("**") == 0 ){ //returns 0 if the same
+        return true;
+    }
+    return false;
 }
 
-bool U(void){
+bool L(){
+    // L -> D | DL
+    if(D()){
+        if(buff.empty() == false){
+            if(L()){
+                return true;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+bool U(){
     // U -> + | - | !
-    if(buff[0] == '+' || buff[0] == '-' || buff[0] == '!'){
-        buff = buff.substr(1);
+    if(buff[0] == '+'){
+        return true;
+    }
+    else if (buff[0] == '-'){
+        return true;
+    }
+    else if (buff[0] == '!'){
         return true;
     }
     return false;
 }
 
-bool D(void){
+bool D(){
     // D -> 0 | 1 | ... | 8 | 9
-    if(0 <= buff[0] && buff[0] <= 9){
+    if('0' <= buff[0] && buff[0] <= '9'){
         buff = buff.substr(1);
         return true;
     }
     return false;
 }
-
-//notes : static binding is a fixed variable declaration 
-//      : dynamic binding happens when the program runse2 
